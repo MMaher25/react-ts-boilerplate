057ff772f9e2f1b445b0d757378e78bba87c6abd
Mark Maher <MMaher@PunctualAbstract.com>
2019-11-27 14:54:16 -0600

    Added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext, useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { hot } from 'react-hot-loader/root';
 import { Store } from './Store/Store'
 import { fetchDataAction } from './Actions/Actions'
@@ -9,13 +9,14 @@ const App: React.FC = (): JSX.Element => {
   const { state, dispatch } = useContext(Store)
 
   useEffect(() => {
+    // Dispatches the fetchDataAction if the pokemon's name is an empty string
     state.pokemon.name === '' && fetchDataAction(dispatch)
   })
 
   return (
-    <Fragment>
+    <div className="flex-container">
       <Pokemon />
-    </Fragment>
+    </div>
   )
 }
 
--- src/Components/Pokemon.tsx
+++ src/Components/Pokemon.tsx
@@ -1,12 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, Fragment } from 'react'
 import { Store } from '../Store/Store'
+import { fetchDataAction } from '../Actions/Actions'
 
 export default function Pokemon() {
   const { state, dispatch } = useContext(Store)
+  const { pokemon } = state
 
   return (
-    <div>
-      {state.pokemon.name}
+    <div className="flex-item">
+      <div>
+        <img src={pokemon.sprites.front_default} />
+      </div>
+      <div>
+        {pokemon.name.toUpperCase()} #{pokemon.id}
+      </div>
+      <button onClick={() => fetchDataAction(dispatch)}>
+        Get Random Pokemon
+      </button>
     </div>
   )
 }
